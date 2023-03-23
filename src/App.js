f5e22487ec31c4f0fd5b6b78eecf9ebffb988a0d
sarmad505 <sarmadbaig505@gmail.com>
2023-03-23 23:09:31 +0500

    remove comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Slider from './components/Slider';
+import Sidebar from './components/Slider/Sidebar';
 
 const App = () => {
 
   return (
     <div className="app">
       <Slider />
+      <Sidebar/>
     </div>
   );
 };
