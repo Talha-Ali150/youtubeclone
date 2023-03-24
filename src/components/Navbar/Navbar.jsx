0179bb71eaf33c6f48ec952c03eb46da45ac0b92
Areebey <areebrafiq125@gmail.com>
2023-03-25 03:35:38 +0500

    make navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import logo from "../../logo.png";
+import {Link} from "react-router-dom"
 import "./Navbar.css";
 import {RxHamburgerMenu,RxAvatar} from "react-icons/rx";
 import {BsSearch} from "react-icons/bs";
 import {MdKeyboardVoice} from "react-icons/md";
 import {SlOptionsVertical} from "react-icons/sl";
 
+
 const Navbar = () => {
 
   return (
+
   <nav className="navbar">
+   
   <RxHamburgerMenu/>
   <img  src={logo} alt="youtube logo" />
   <input placeholder="Search" type="text" />
   <button className="search-btn" >{<BsSearch/>}</button>
+
   <MdKeyboardVoice/>
   <button className='option-btn'>{<SlOptionsVertical/>}</button> 
    <button className="signin-btn">{<RxAvatar/>}Sign In</button>
